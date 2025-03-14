1cc7eb024b77c3526a70dfcc9c3214a115b054a8
OluwaMarg <oluwadaremargie@gmail.com>
2025-03-14 12:57:51 +0100

    Wk5 Course work
    Course work on Data cleaning and ML. To be submitted on 19-March-2025

---
+++ Classwork_Assignments/Wk5 Coursework/Hands-on assignment.py
@@ -0,0 +1,14 @@
+# QUESTION  1: Following from the last code chunck in our case study, 
+
+# Repeat the same steps for the test set by inserting your code here
+
+
+
+
+##QUESTION 2: Run a full maachine learning on the Ames Husing data set. Noote you can refer to out note on week 3
+
+
+
+
+
+
